e1e4f767742713642b07bd7a3077864124153f63
chanon-mike <chanon-mike@users.noreply.github.com>
2023-05-29 04:30:03 +0000

    deploy: ee311a1b6cf8826d93187a7789fd6e7e9841ac02

---
+++ _next/static/CndRuDG-KBFiUYauP-enw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/cd096809b7128afc.css","static/chunks/pages/index-76f61e20915038ad.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
