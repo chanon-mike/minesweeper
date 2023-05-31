aa96f5658ccc4e1cd1dd78518444497988d75eba
chanon-mike <chanon-mike@users.noreply.github.com>
2023-05-31 16:20:43 +0000

    deploy: eed9d5b27b3df1cbbd2e91b87dafc5bbae021356

---
+++ _next/static/9sYpJzBlydrW6s0xO-aal/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/cd096809b7128afc.css","static/chunks/pages/index-e0e0039b6d896bc0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
