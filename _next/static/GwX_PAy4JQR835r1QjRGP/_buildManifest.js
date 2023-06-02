087b5ff8b1992af6685428ea486d724196ec0db1
chanon-mike <chanon-mike@users.noreply.github.com>
2023-06-02 15:08:55 +0000

    deploy: 5844c8091d317ad4b9b4d4bd16b9dd3843c096ee

---
+++ _next/static/GwX_PAy4JQR835r1QjRGP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/acff97f5db3ac7ca.css","static/chunks/pages/index-a010963b04ad96b2.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
